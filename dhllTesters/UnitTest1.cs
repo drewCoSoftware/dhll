fbd2449d19cf0d1b10ee1d49977ddf8689ad1caf
Andrew A. Ritz <drew@august-harper.com>
2025-02-23 22:43:04 -0500

    - Project file reorganizations.

---
+++ dhllTesters/UnitTest1.cs
@@ -0,0 +1,17 @@
+
+//namespace dhllTesters
+//{
+//  public class Tests
+//  {
+//    [SetUp]
+//    public void Setup()
+//    {
+//    }
+
+//    [Test]
+//    public void Test1()
+//    {
+//      Assert.Pass();
+//    }
+//  }
+//}
