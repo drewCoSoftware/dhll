e3653b180cbd47670d4bc1089e39f0b319f6a9d8
Andrew A. Ritz <drew@august-harper.com>
2025-03-07 11:44:42 -0500

    - Fixed a test case. - Added a def for as type of project file, and some other test inputs   that we can experiment with.

---
+++ dhll/dhlprojFile.cs
@@ -0,0 +1,91 @@
+﻿using drewCo.Curations;
+using drewCo.Tools;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace dhll;
+// ==============================================================================================================================
+/// <summary>
+/// A JSON file that describes a 'project' composed of many dhll files.
+/// All paths use unix stlye separators and are relative to this file.
+/// </summary>
+public class dhlprojFile
+{
+  /// <summary>
+  /// The path of this file.
+  /// </summary>
+  [JsonIgnore]
+  public string Path { get; private set; } = null!;
+
+  ExternalReadonlyList<string> _Files = new ExternalReadonlyList<string>();
+  public ReadOnlyCollection<string> Files { get { return _Files.External; } }
+
+  // --------------------------------------------------------------------------------------------------------------------------
+  [JsonConstructor]
+  private dhlprojFile() { }
+
+  // --------------------------------------------------------------------------------------------------------------------------
+  public dhlprojFile(string path_)
+  {
+    if (path_ == null) { throw new ArgumentNullException(nameof(path_)); }
+
+    this.Path = FileTools.GetRootedPath(path_);
+    Save();
+  }
+
+  // --------------------------------------------------------------------------------------------------------------------------
+  public void Save()
+  {
+    if (this.Path == null)
+    {
+      throw new InvalidOperationException("Project file has no path set!");
+    }
+
+    string? dir = System.IO.Path.GetDirectoryName(this.Path);
+    if (dir == null) { throw new InvalidOperationException($"Could not resolve the directory from path: {Path}!"); }
+
+    FileTools.CreateDirectory(dir);
+    FileTools.SaveJson<dhlprojFile>(this.Path, this);
+  }
+
+  // --------------------------------------------------------------------------------------------------------------------------
+  public void AddFile(string path)
+  {
+    if (this.Path == null)
+    {
+      throw new InvalidOperationException("Project file has no path set!");
+    }
+
+    string relPath = FileTools.GetRelativePath(this.Path, path);
+    _Files.Add(relPath);
+  }
+
+  // --------------------------------------------------------------------------------------------------------------------------
+  public static dhlprojFile Load(string path)
+  {
+    if (!File.Exists(path)) { throw new FileNotFoundException($"The file at path: {path} does not exist!"); }
+
+    var res = FileTools.LoadJson<dhlprojFile>(path);
+    if (res == null)
+    {
+      throw new InvalidOperationException($"The file at path: {path} could not be deserialized into an instance of: {typeof(dhlprojFile)}!");
+    }
+    res.Path = path;
+
+    return res;
+  }
+
+}
+
+// ==============================================================================================================================
+public class dllprojFile
+{
+  public string Path { get; set; }
+  public string FileType { get; set; }
+}
---
+++ dhllTesters/TestBase.cs
@@ -0,0 +1,40 @@
+﻿
+using drewCo.Tools;
+
+namespace dhllTesters
+{
+  // ==============================================================================================================================
+  public class TestBase
+  {
+    protected string TestDir = null!;
+
+    // --------------------------------------------------------------------------------------------------------------------------
+    public TestBase()
+    {
+      TestDir = FileTools.GetLocalDir("TestInputs");
+    }
+
+    // --------------------------------------------------------------------------------------------------------------------------
+    protected string GetTestInputPath(string name)
+    {
+      string res = Path.Combine(TestDir, name);
+
+      return res;
+    }
+
+    // --------------------------------------------------------------------------------------------------------------------------
+    protected string LoadTestInput(string name)
+    {
+      string inputPath = GetTestInputPath(name);  
+      if (!File.Exists(inputPath))
+      {
+        throw new FileNotFoundException($"The input file at path: {inputPath} does not exist!");
+      }
+
+      string data = File.ReadAllText(inputPath);
+      return data;
+    }
+  }
+
+
+}
